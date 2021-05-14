0e3e24595bb373899e02bf9434b73144135e603d
Mirko Da Corte <mirko@etherna.io>
2021-05-15 00:47:09 +0200

    Added port range in BeeNodeInput model

--- src/BeehiveManager/Areas/Api/InputModels/BeeNodeInput.cs
+++ src/BeehiveManager/Areas/Api/InputModels/BeeNodeInput.cs
@@ -4,8 +4,12 @@ namespace Etherna.BeehiveManager.Areas.Api.InputModels
 {
     public class BeeNodeInput
     {
+        [Range(1, 65535)]
         public int? DebugApiPort { get; set; }
+
+        [Range(1, 65535)]
         public int? GatwayApiPort { get; set; }
+
         [Required]
         public string Url { get; set; } = default!;
     }
