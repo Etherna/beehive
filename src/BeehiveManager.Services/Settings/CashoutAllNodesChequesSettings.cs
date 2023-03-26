a8266af611f39ae52be90ff288b7f53e15a61453
Mirko Da Corte <mirko@etherna.io>
2023-03-26 19:58:59 +0200

    add cashout task config

---
+++ src/BeehiveManager.Services/Settings/CashoutAllNodesChequesSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace Etherna.BeehiveManager.Services.Settings
+{
+    public class CashoutAllNodesChequesSettings
+    {
+        // Consts.
+        public const string ConfigPosition = "CashoutCheques";
+
+        // Properties.
+        public decimal BzzMaxTrigger { get; set; } = 1;
+    }
+}
