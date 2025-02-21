5ea8e3d84e6c8be83cb86fca0eef5d744c2dfa3f
Mirko Da Corte <mirko@etherna.io>
2025-02-21 18:41:52 +0100

    Add chunk-saving buffer with flush mechanism and dispose

--- src/Beehive.Services/Utilities/BeehiveChunkStore.cs
+++ src/Beehive.Services/Utilities/BeehiveChunkStore.cs
@@ -19,8 +19,10 @@ using Etherna.BeeNet.Stores;
 using Etherna.MongoDB.Driver.GridFS;
 using Etherna.MongODM.Core.Utility;
 using System;
+using System.Collections.Concurrent;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Etherna.Beehive.Services.Utilities
@@ -29,17 +31,75 @@ namespace Etherna.Beehive.Services.Utilities
     public sealed class BeehiveChunkStore(
         IBeeNodeLiveManager beeNodeLiveManager,
         IBeehiveDbContext dbContext,
+        int chunkSavingBufferSize = 0,
         Action<Chunk>? onSavingChunk = null)
-        : ChunkStoreBase
+        : ChunkStoreBase, IDisposable
     {
+        // Fields.
+        private readonly ConcurrentDictionary<SwarmHash, Chunk> chunkSavingBuffer = new();
+        private readonly SemaphoreSlim flushSemaphore = new(1, 1);
+        
+        private bool disposed;
+        
+        // Destructor and dispose.
+        ~BeehiveChunkStore() => Dispose(false);
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+        private void Dispose(bool disposing)
+        {
+            if (disposed) return;
+
+            // Dispose managed resources.
+            if (disposing)
+                flushSemaphore.Dispose();
+
+            disposed = true;
+        }
+        
         // Methods.
+        public async Task FlushSaveAsync()
+        {
+            /*
+             * Use flush semaphore to avoid concurrent flushing requests.
+             * This could happen if a burst of chunks is wrote faster than the buffer to flush chunks.
+             * Each time that the buffer size is larger than the max size, flush is invoked. But if another flush
+             * is already operating, we don't want to split the flush work in different db requests.
+             *
+             * Wait and don't skip because exiting from a flush we need to be sure that all the already present chunks
+             * have been written on db.
+             */
+            await flushSemaphore.WaitAsync();
+
+            try
+            {
+                // Don't create on db with no chunks.
+                var chunks = chunkSavingBuffer.Values.ToArray();
+                if (chunks.Length == 0)
+                    return;
+                
+                // Write on db before to remove from buffer. This permits to have chunks available always.
+                await dbContext.Chunks.CreateAsync(chunks); 
+                foreach (var chunk in chunks)
+                    chunkSavingBuffer.TryRemove(chunk.Id, out _);
+            }
+            finally
+            {
+                flushSemaphore.Release();
+            }
+        }
+        
+        // Protected methods.
         protected override async Task<bool> DeleteChunkAsync(SwarmHash hash)
         {
             using var dbExecContextHandler = new DbExecutionContextHandler(dbContext);
 
-            var found = false;
+            // Try remove from buffer.
+            var found = chunkSavingBuffer.TryRemove(hash, out _);
             
-            //try remove from repository
+            // Try remove from repository.
             var chunk = await dbContext.Chunks.TryFindOneAsync(c => c.Hash == hash);
             if (chunk is not null)
             {
@@ -47,7 +107,7 @@ namespace Etherna.Beehive.Services.Utilities
                 await dbContext.Chunks.DeleteAsync(chunk);
             }
             
-            //try remove from old gridfs
+            // Try remove from old gridfs.
             try
             {
                 using var downStream = await dbContext.ChunksBucket.OpenDownloadStreamByNameAsync(hash.ToString());
@@ -59,10 +119,17 @@ namespace Etherna.Beehive.Services.Utilities
 
             return found;
         }
-
+        
         protected override async Task<SwarmChunk> LoadChunkAsync(SwarmHash hash)
         {
-            // Try load from db first.
+            // Try to find on buffer.
+            if (chunkSavingBuffer.TryGetValue(hash, out var chunk))
+            {
+                var payload = chunk.Payload.ToArray();
+                return SwarmChunk.BuildFromSpanAndData(hash, payload);
+            }
+            
+            // Try load from db.
             using(var _ = new DbExecutionContextHandler(dbContext))
             {
                 //try to find on repository
@@ -93,15 +160,23 @@ namespace Etherna.Beehive.Services.Utilities
         {
             ArgumentNullException.ThrowIfNull(chunk, nameof(chunk));
             
-            using var dbExecContextHandler = new DbExecutionContextHandler(dbContext);
-            
             try
             {
                 var domainChunk = new Chunk(chunk.Hash, chunk.GetSpanAndData());
 
                 onSavingChunk?.Invoke(domainChunk);
+
+                if (chunkSavingBufferSize > 0)
+                {
+                    chunkSavingBuffer.TryAdd(chunk.Hash, domainChunk);
+                    if (chunkSavingBuffer.Count >= chunkSavingBufferSize)
+                        await FlushSaveAsync();
+                }
+                else
+                {
+                    await dbContext.Chunks.CreateAsync(domainChunk);
+                }
                 
-                await dbContext.Chunks.CreateAsync(domainChunk);
                 return true;
             }
             catch
