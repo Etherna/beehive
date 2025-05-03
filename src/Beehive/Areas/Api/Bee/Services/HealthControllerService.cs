04acbf96d62fc9e4c9aa5d1975cff5bbdbaba718
Mirko Da Corte <mirko@etherna.io>
2025-05-03 20:44:36 +0200

    add bee status apis

---
+++ src/Beehive/Areas/Api/Bee/Services/HealthControllerService.cs
@@ -0,0 +1,28 @@
+// Copyright 2021-present Etherna SA
+// This file is part of Beehive.
+// 
+// Beehive is free software: you can redistribute it and/or modify it under the terms of the
+// GNU Affero General Public License as published by the Free Software Foundation,
+// either version 3 of the License, or (at your option) any later version.
+// 
+// Beehive is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+// See the GNU Affero General Public License for more details.
+// 
+// You should have received a copy of the GNU Affero General Public License along with Beehive.
+// If not, see <https://www.gnu.org/licenses/>.
+
+using Etherna.Beehive.Areas.Api.Bee.DtoModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Etherna.Beehive.Areas.Api.Bee.Services
+{
+    public class HealthControllerService : IHealthControllerService
+    {
+        public IActionResult GetHealthStatus() =>
+            new JsonResult(new HealthDto(
+                "ok",
+                typeof(HealthControllerService).Assembly.GetName().Version?.ToString() ?? "0.0.0",
+                "beehive-0.0.0"));
+    }
+}
